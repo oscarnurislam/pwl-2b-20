306d2a9c32363ca0eacbce1f6e69443ed7937987
oscarnurislam <oscarnurislam694@gmail.com>
2021-04-13 15:39:17 +0700

    Ubah UTS

--- src/08_uts/resources/views/dashboard.blade.php
+++ src/08_uts/resources/views/dashboard.blade.php
@@ -87,38 +87,3 @@
     </div>
 
     
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-        <div class="col-md-12 mt-5">
-            <div class="card">
-                <div class="card-header">
-                    <h3>Dashboard</h3>
-                </div>
-                <div class="card-body">
-                
-                    <h1 align="center">Ini Adalah Identitas-mu</h1> 
-                    <br>
-                    <table class="table table-responsive"> 
-                    
-                    <tr><th>Name</th><th>:</th><td>{{ Auth::user()->name }}</td></tr>
-                    <tr><th>Email</th><th>:</th><td>{{ Auth::user()->email }}</td></tr>
-                    <tr><th>Created At</th><th>:</th><td>{{ Auth::user()->created_at }}</td></tr>
-                    </table>
-                    
-                    <br>
-                    <a href="{{ route('logout') }}" class="btn btn-danger">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-</html> -->
